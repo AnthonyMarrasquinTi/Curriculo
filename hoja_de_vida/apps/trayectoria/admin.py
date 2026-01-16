1b303c602dacdd7207eb85c5e9036a819d7b475a
jaAnthonyja <e1350196570@live.uleam.edu.ec>
2026-01-16 00:41:14 -0500

    Fix admin forms for CursoRealizado y Reconocimiento

--- hoja_de_vida/apps/trayectoria/admin.py
+++ hoja_de_vida/apps/trayectoria/admin.py
@@ -1,4 +1,3 @@
-from django import forms
 from django.contrib import admin, messages
 from django.utils.translation import gettext_lazy as _
 from .models import (
@@ -9,28 +8,8 @@ from .models import (
     ProductoLaboral,
     VentaGarage,
 )
+from .forms_admin import CursoRealizadoAdminForm, ReconocimientoAdminForm
 from .services.azure_storage import upload_pdf
-
-
-class CertificadoUploadForm(forms.ModelForm):
-    certificado_subir = forms.FileField(required=False, label=_('Certificado (PDF)'))
-
-    class Meta:
-        model = None  # will be set dynamically in ModelAdmin
-        fields = '__all__'
-
-    def clean_certificado_subir(self):
-        f = self.cleaned_data.get('certificado_subir')
-        if not f:
-            return f
-        # Validate content_type if available
-        content_type = getattr(f, 'content_type', None)
-        name = getattr(f, 'name', '')
-        if content_type and content_type != 'application/pdf':
-            raise forms.ValidationError(_('Sólo se aceptan archivos PDF (content-type inválido).'))
-        if not name.lower().endswith('.pdf'):
-            raise forms.ValidationError(_('El archivo debe tener extensión .pdf'))
-        return f
 
 
 @admin.register(ExperienciaLaboral)
@@ -40,13 +19,8 @@ class ExperienciaLaboralAdmin(admin.ModelAdmin):
 
 @admin.register(Reconocimiento)
 class ReconocimientoAdmin(admin.ModelAdmin):
+    form = ReconocimientoAdminForm
     list_display = ('descripcionreconocimiento', 'activarparaqueseveaenfront')
-
-    def get_form(self, request, obj=None, **kwargs):
-        # attach the model to the dynamic form
-        form = type('DynamicReconocimientoForm', (CertificadoUploadForm,), {})
-        form.Meta.model = Reconocimiento
-        return form
 
     def save_model(self, request, obj, form, change):
         # If a file was uploaded, send to Azure and save URL
@@ -59,12 +33,8 @@ class ReconocimientoAdmin(admin.ModelAdmin):
 
 @admin.register(CursoRealizado)
 class CursoRealizadoAdmin(admin.ModelAdmin):
+    form = CursoRealizadoAdminForm
     list_display = ('nombrecurso', 'activarparaqueseveaenfront')
-
-    def get_form(self, request, obj=None, **kwargs):
-        form = type('DynamicCursoForm', (CertificadoUploadForm,), {})
-        form.Meta.model = CursoRealizado
-        return form
 
     def save_model(self, request, obj, form, change):
         uploaded = form.cleaned_data.get('certificado_subir')
