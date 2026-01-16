0d6e100b6dfdb6cf8783a141fc5d6cc2929ce273
jaAnthonyja <e1350196570@live.uleam.edu.ec>
2026-01-16 01:45:36 -0500

    configure postgres correctly

--- hoja_de_vida/config/settings.py
+++ hoja_de_vida/config/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import dj_database_url
 import os
 
 # =========================================================
@@ -90,10 +91,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 # =========================================================
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+        ssl_require=False
+    )
 }
 
 # =========================================================
